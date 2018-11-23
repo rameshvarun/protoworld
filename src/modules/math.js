5c1bb67248b57efb3b0a41dbadc8ebe45188c8cb
Varun Ramesh <varunramesh@live.com>
2018-11-23 15:39:19 -0700

    Vec3 improvements.

--- src/modules/math.js
+++ src/modules/math.js
@@ -1,3 +1,9 @@
+/*
+MathModule - ProtoWorld Module
+Object-oriented Math utilities, include Vector and Matrix classes.
+*/
+
+/* BEGIN MODULE PRELUDE */
 let ref = function(path) {
   var parts = path.split(".");
   var current = window;
@@ -7,37 +13,25 @@ let ref = function(path) {
   return current;
 };
 
-let slot = function(path, name, value) {
+let slot = function(path, name, value, annotations = {}) {
   _AddSlot(ref(path), name, value);
+  for (let annotation in annotations) {
+    _SetSlotAnnotation(ref(path), name, annotation, annotations[annotation]);
+  }
+  _SetSlotAnnotation(ref(path), name, "module", mod);
 };
 
-slot(
-  "World.Modules",
-  "math",
-  (function() {
-    let object = ref("World.Modules.math");
-    _SetAnnotation(object, "name", `MathModule`);
-    _SetAnnotation(object, "creator", ref("World.Modules"));
-    _SetAnnotation(object, "creatorSlot", `math`);
+let prototype_slot = function(path, name, value, annotations = {}) {
+  slot(path, name, value, annotations);
+  _AddPrototypeSlot(ref(path), name);
+};
 
-    return object;
-  })()
-);
-_SetSlotAnnotation(
-  ref("World.Modules"),
-  "math",
-  "module",
-  ref("World.Modules.math")
-);
+let msg = function(code) {
+  return _MakeMessageHandler(code);
+};
+/* END MODULE PRELUDE */
 
-slot("World.Modules.math", "parent", ref("World.Core.Module"));
-_AddPrototypeSlot(ref("World.Modules.math"), "parent");
-_SetSlotAnnotation(
-  ref("World.Modules.math"),
-  "parent",
-  "module",
-  ref("World.Modules.math")
-);
+let mod = ref("World.Modules.math");
 
 slot(
   "World",
@@ -52,16 +46,6 @@ slot(
     return object;
   })()
 );
-_SetSlotAnnotation(ref("World"), "Math", "module", ref("World.Modules.math"));
-
-slot("World.Math", "parent", ref("World.Core.Namespace"));
-_AddPrototypeSlot(ref("World.Math"), "parent");
-_SetSlotAnnotation(
-  ref("World.Math"),
-  "parent",
-  "module",
-  ref("World.Modules.math")
-);
 
 slot(
   "World.Math",
@@ -74,99 +58,50 @@ slot(
     return object;
   })()
 );
-_SetSlotAnnotation(
-  ref("World.Math"),
-  "Vec2",
-  "module",
-  ref("World.Modules.math")
-);
 
-slot("World.Math.Vec2", "parent", ref("World.Core.TopObject"));
-_AddPrototypeSlot(ref("World.Math.Vec2"), "parent");
-_SetSlotAnnotation(
-  ref("World.Math.Vec2"),
-  "parent",
-  "module",
-  ref("World.Modules.math")
-);
-
-slot("World.Math.Vec2", "x", 0);
-_SetSlotAnnotation(
-  ref("World.Math.Vec2"),
-  "x",
-  "module",
-  ref("World.Modules.math")
-);
-
-slot("World.Math.Vec2", "y", 0);
-_SetSlotAnnotation(
-  ref("World.Math.Vec2"),
-  "y",
-  "module",
-  ref("World.Modules.math")
+slot(
+  "World.Math.Vec2",
+  "Add",
+  msg(`function(other) {
+    return World.Math.Vec2.FromXY(this.x + other.x, this.y + other.y);
+}`)
 );
 
 slot(
   "World.Math.Vec2",
   "Length",
-  _MakeMessageHandler(`function() {
+  msg(`function() {
     return Math.sqrt(this.x * this.x + this.y * this.y);
 }`)
 );
-_SetSlotAnnotation(
-  ref("World.Math.Vec2"),
-  "Length",
-  "module",
-  ref("World.Modules.math")
-);
 
 slot(
   "World.Math.Vec2",
-  "FromXY",
-  _MakeMessageHandler(`function(x, y) {
+  "New",
+  msg(`function(x, y) {
     let inst = this.Extend();
     inst.x = x;
     inst.y = y;
     return inst;
 }`)
 );
-_SetSlotAnnotation(
-  ref("World.Math.Vec2"),
-  "FromXY",
-  "module",
-  ref("World.Modules.math")
-);
-
-slot(
-  "World.Math.Vec2",
-  "Add",
-  _MakeMessageHandler(`function(other) {
-    return World.Math.Vec2.FromXY(this.x + other.x, this.y + other.y);
-}`)
-);
-_SetSlotAnnotation(
-  ref("World.Math.Vec2"),
-  "Add",
-  "module",
-  ref("World.Modules.math")
-);
 
 slot(
   "World.Math.Vec2",
   "ToString",
-  _MakeMessageHandler(`function() {
+  msg(`function() {
     if (this === World.Math.Vec2)
 	    return "Vec2";
 	else
 	    return \`<\${this.x}, \${this.y}>\`;
 }`)
 );
-_SetSlotAnnotation(
-  ref("World.Math.Vec2"),
-  "ToString",
-  "module",
-  ref("World.Modules.math")
-);
+
+prototype_slot("World.Math.Vec2", "parent", ref("World.Core.TopObject"));
+
+slot("World.Math.Vec2", "x", 0);
+
+slot("World.Math.Vec2", "y", 0);
 
 slot(
   "World.Math",
@@ -180,42 +115,66 @@ slot(
     return object;
   })()
 );
-_SetSlotAnnotation(
-  ref("World.Math"),
-  "Vec3",
-  "module",
-  ref("World.Modules.math")
+
+slot(
+  "World.Math.Vec3",
+  "IsInteger",
+  msg(`function() {
+    return Number.isInteger(this.x) &&
+        Number.isInteger(this.y) &&
+        Number.isInteger(this.z);
+}`)
 );
 
-slot("World.Math.Vec3", "parent", ref("World.Core.TopObject"));
-_AddPrototypeSlot(ref("World.Math.Vec3"), "parent");
-_SetSlotAnnotation(
-  ref("World.Math.Vec3"),
-  "parent",
-  "module",
-  ref("World.Modules.math")
+slot(
+  "World.Math.Vec3",
+  "New",
+  msg(`function(x, y, z) {
+    let inst = this.Extend();
+    inst.x = x;
+    inst.y = y;
+    inst.z = z;
+    return inst;
+}`)
 );
+
+slot(
+  "World.Math.Vec3",
+  "ToString",
+  msg(`function() {
+    if (this === World.Math.Vec3)
+	    return "Vec3";
+	else
+	    return \`<\${this.x}, \${this.y}, \${this.z}>\`;
+}`)
+);
+
+prototype_slot("World.Math.Vec3", "parent", ref("World.Core.TopObject"));
 
 slot("World.Math.Vec3", "x", 0);
-_SetSlotAnnotation(
-  ref("World.Math.Vec3"),
-  "x",
-  "module",
-  ref("World.Modules.math")
-);
 
 slot("World.Math.Vec3", "y", 0);
-_SetSlotAnnotation(
-  ref("World.Math.Vec3"),
-  "y",
-  "module",
-  ref("World.Modules.math")
-);
 
 slot("World.Math.Vec3", "z", 0);
-_SetSlotAnnotation(
-  ref("World.Math.Vec3"),
-  "z",
-  "module",
-  ref("World.Modules.math")
+
+prototype_slot("World.Math", "parent", ref("World.Core.Namespace"));
+
+slot(
+  "World.Modules",
+  "math",
+  (function() {
+    let object = ref("World.Modules.math");
+    _SetAnnotation(object, "name", `MathModule`);
+    _SetAnnotation(object, "creator", ref("World.Modules"));
+    _SetAnnotation(object, "creatorSlot", `math`);
+    _SetAnnotation(
+      object,
+      "description",
+      `Object-oriented Math utilities, include Vector and Matrix classes.`
+    );
+
+    return object;
+  })()
 );
+
+prototype_slot("World.Modules.math", "parent", ref("World.Core.Module"));
