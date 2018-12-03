b83828eccde82d65a92129b139c2098e1f7e6c32
Varun Ramesh <varunramesh@live.com>
2018-12-02 17:53:15 -0800

    Don't reparse userAgent every render.

--- src/modules/interface.js
+++ src/modules/interface.js
@@ -108,7 +108,9 @@ slot(
       External.LoadCSS('https://use.fontawesome.com/releases/v5.0.10/css/all.css',
         'sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg'),
       External.LoadScript('https://cdnjs.cloudflare.com/ajax/libs/mobile-detect/1.4.3/mobile-detect.js',
-        'sha256-qRwMbhq9rGx6VsrTZu8+Adq4w23C0FzpEhqOo0J147A='),
+        'sha256-qRwMbhq9rGx6VsrTZu8+Adq4w23C0FzpEhqOo0J147A=').then(() => {
+          window.MobileDetect = new MobileDetect(window.navigator.userAgent);
+        }),
       External.LoadScript('https://cdnjs.cloudflare.com/ajax/libs/node-uuid/1.4.8/uuid.js',
         'sha256-di30dL7N9597Q8UApQWE7AIjWlR2hbVHreqxh7NiX4I='),
       External.LoadScript('https://unpkg.com/file-saver@2.0.0/dist/FileSaver.min.js',
@@ -430,7 +432,7 @@ slot(
   "World.Interface.Window",
   "Render",
   msg(`function() {
-  let isMobile = new MobileDetect(window.navigator.userAgent).mobile() !== null;
+  let isMobile = MobileDetect.mobile() !== null;
 
   let windowStyle = isMobile ? {
         border: "1px solid black",
@@ -859,7 +861,7 @@ slot(
   "World.Interface.MainMenu",
   "Render",
   msg(`function() {
-  let isMobile = new MobileDetect(window.navigator.userAgent).mobile() !== null;
+  let isMobile = MobileDetect.mobile() !== null;
   let barStyle = isMobile ? {
      'backgroundColor': '#285477',
      maxWidth: '100%',
